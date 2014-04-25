ee66c196938785fd3fd576e43abc2c5a184e2433
Yao-Yuan Mao <maomao.321@gmail.com>
2014-04-25 12:14:39 -0700

    remove pandas dependency

--- examples/main_catalog.py
+++ examples/main_catalog.py
@@ -1,18 +1,39 @@
+import numpy as np
 from urllib2 import urlopen
-import pandas as pd
 
-url = "https://raw.githubusercontent.com/saga-survey/saga-data/master/masterlist.csv"
-d = pd.read_csv(urlopen(url), skiprows=[1])
+def get_raw_data():
+    url = "https://raw.githubusercontent.com/saga-survey/saga-data/master/masterlist.csv"
+    names = "RA,Dec,PGC,NSAID,othername,vhelio,vhelio_err,distance,r,i,z,I,K".split(',')
+    usecols = (3,2,7,8,9,10,11,12,5,6,0,1)
+    fmts = [float]*len(names)
+    conv = dict(zip(range(len(names)), [lambda s : float(s or np.nan)]*len(names)))
+    for i in [2, 3]:
+        fmts[i] = int
+        conv[i] = lambda s : int(s or -1)
+    dtype = zip(names, fmts)
+    dtype = [dtype[i] for i in usecols]
+    conv = {i:conv[i] for i in usecols}
+    return np.loadtxt(urlopen(url), np.dtype(dtype), delimiter = ',', converters=conv, skiprows=2, usecols=usecols)
 
 def apply_cut(df, cut_f, *fields):
     return df[cut_f(*map(df.__getitem__, fields))]
 
-dc = d
-dc = apply_cut(dc, lambda x: ~x.isnull(), 'NSAID')
+dc = get_raw_data()
+
+#change cuts here
+dc = apply_cut(dc, lambda x: x>-1, 'NSAID')
 dc = apply_cut(dc, lambda x: x>7, 'K')
 dc = apply_cut(dc, lambda x: x<10, 'K')
 dc = apply_cut(dc, lambda x: x>20, 'distance')
 dc = apply_cut(dc, lambda x: x<45, 'distance')
-dc.sort(columns='K', inplace=True)
-dc.to_csv('my_catalog.csv', cols=['NSAID', 'PGC#', 'distance', 'r','i','z','I','K','vhelio','vhelio_err'], float_format="%g", index=False)
 
+dc = dc[dc['K'].argsort()]
+s = slice(0, -2) #to exclude RA and Dec
+
+def tostr(v):
+    return '' if v is -1 or np.isnan(v) else '%g'%v
+
+with open('main_catalog.csv', 'w') as f:
+    f.write(','.join(dc.dtype.names[s]) + '\n')
+    for r in dc:
+        f.write(','.join(map(tostr, r.tolist()[s])) + '\n')
